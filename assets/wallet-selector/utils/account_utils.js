1d2e42d14840df4899591650a39f07a1a535c7b9
ArturLevchuk <levchukarturr@gmail.com>
2024-08-28 14:42:27 +0300

    fix: getAccount accountId null check

--- assets/wallet-selector/utils/account_utils.js
+++ assets/wallet-selector/utils/account_utils.js
@@ -3,7 +3,11 @@ import * as nearAPI from "near-api-js";
 
 export async function getAccount() {
   const network = window.selector.options.network.networkId;
-  const accountId = window.selector.store.getState().accounts[0].accountId;
+  const accountId = window.selector.store.getState().accounts[0]?.accountId;
+
+  if (!accountId || !network) {
+    return null;
+  }
 
   const keyPair = await getKeyPair(network, accountId);
 
